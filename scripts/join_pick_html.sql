c9e4d1c82079e9e4bb5fa6cb10faae026f0b9eed
Kelv Gooding <82043281+kelvgooding@users.noreply.github.com>
2023-02-19 21:05:55 +0000

    Delete scripts directory

--- scripts/join_pick_html.sql
+++
@@ -1,6 +0,0 @@
--- pick.html - This will join group code and gn_horses together
-
-SELECT BOMXBX.status, BOMXBX.full_name, gn_horses.h_num, gn_horses.h_name, gn_horses.h_odds, gn_horses.ranked
-FROM BOMXBX
-LEFT JOIN gn_horses
-ON BOMXBX.grp_rank = gn_horses.h_code;
--- scripts/validate_group_code.sql
+++
@@ -1,3 +0,0 @@
--- index.html - When the form is submitted, this is to check will search for the existence of the group code.
-
-select groupid from ss_group where groupid = "groupcode";
